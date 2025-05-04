87d643b145933d9f2ff3497b4aa3b59a35d40b61
anirudh <anirudh>
2025-05-04 11:14:57 +0530

    Trigger  pipeline

--- infra/aws/backend.tf
+++ infra/aws/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "consul" {
-    address = " https://f3eb-2405-201-f008-70b2-14da-e28-a352-adb3.ngrok-free.app"  # Correct HTTP API URL
+    address = "https://f3eb-2405-201-f008-70b2-14da-e28-a352-adb3.ngrok-free.app"  # Correct HTTP API URL
     path    = "terraform/state"  # Path to store the state in Consul
   }
 }
